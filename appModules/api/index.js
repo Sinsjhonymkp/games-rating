959cbf68ddd19485242a026a73b4cadbe445950a
Sinsjhonymkp <tohaantoha96@gmail.com>
2024-05-02 13:44:16 +0300

    review

---
+++ appModules/api/index.js
@@ -0,0 +1,5 @@
+const { getData } = require('./api-utilts');
+const endPoints = require('./config');
+const { getRandomGame } = require('./api-utilts')
+
+module.exports = { getData, endPoints, getRandomGame };
